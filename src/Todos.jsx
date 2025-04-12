11b5baf9d8ff941c4ea0802ae9ff69318a255c6a
Hamza-Hashmi024 <hamza064@gmail.com>
2025-04-12 19:50:49 +0500

    basic structure

---
+++ src/Todos.jsx
@@ -0,0 +1,26 @@
+export  const Todos = () => {
+   
+    return (
+      <>
+      <section className="">
+        <header> 
+            Todos App
+        </header>
+      </section>
+      <section>
+        <form>
+          <div>
+            <input type="text" placeholder="Enter your task" autoComplete="off" />
+          </div>
+          <div>
+            <button type="submit">Add Task</button>
+          </div>
+        </form>
+      </section>
+      
+      
+      </>
+    )
+
+
+}
