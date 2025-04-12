8f5d2223b34fa02d66ab02e03ca604899a1627db
Hamza-Hashmi024 <hamza064@gmail.com>
2025-04-12 20:15:17 +0500

    store state data in array

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,26 +1,60 @@
-export  const Todos = () => {
+import { useState } from "react";
+
+export const Todos = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [task ,  setTask] = useState([]);
+
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
+
+
+
+  const handleSubmit = (e) => {
+   e.preventDefault();
+  if(!inputValue) return; // Prevent empty tasks
+  setTask((prevTask ) => [...prevTask , inputValue]);
+
+   setInputValue("");
    
-    return (
-      <>
+}
+
+  return (
+    <>
       <section className="">
-        <header> 
-            Todos App
-        </header>
+        <header>Todos App</header>
       </section>
       <section>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <input type="text" placeholder="Enter your task" autoComplete="off" />
+            <input
+              type="text"
+              placeholder="Enter your task"
+              autoComplete="off"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
           </div>
           <div>
             <button type="submit">Add Task</button>
           </div>
         </form>
       </section>
-      
-      
-      </>
-    )
 
+      <section>
+        <ul>
+            {task.map((item , index) =>{
+                return(
+                    <li key={index} >
+                        {item}
+                    </li>
+                )
+            })}
+        </ul>
 
-}
+      </section>
+    </>
+  );
+};
